ba1b2931c6eea4090d96e409165d2fbd8ed5d972
Rinat Ibragimov <ibragimovrinat@mail.ru>
2016-01-07 18:01:52 +0300

    conditional logging

--- src/log.hh
+++ src/log.hh
@@ -2,6 +2,12 @@
 
 #include <iostream>
 
+#define DEBUG_TRACE
 
+#ifdef DEBUG_TRACE
 #define LOGF    std::cout << "gmp-widevine/F: "
+#else
+#define LOGF    while (0) std::cout
+#endif // DEBUG_TRACE
+
 #define LOGZ    std::cout << "gmp-widevine/Z: "
